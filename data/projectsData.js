c9b62206991bc6e0b141f09728b366977a48cd91
Ivan Li <ivanli2048@gmail.com>
2022-09-10 18:41:52 +0800

    project(UPS): 添加 UPS 项目。

--- data/projectsData.js
+++ data/projectsData.js
@@ -1,12 +1,10 @@
 const projectsData = [
-  // {
-  //   title: 'A Search Engine',
-  //   description: `What if you could look up any information in the world? Webpages, images, videos
-  //   and more. Google has many features to help you find exactly what you're looking
-  //   for.`,
-  //   imgSrc: '/static/images/google.png',
-  //   href: 'https://www.google.com',
-  // },
+  {
+    title: 'UPS',
+    description: `一个不间断电源（UPS）的全栈项目。核心硬件使用乐鑫×安信可的 ESP32-C3-32S 模块作为主控，软件部分使用了 Rust + ESP-IDF 开发。`,
+    // imgSrc: '/static/images/google.png',
+    href: 'https://git.ivanli.cc/Ivan/ups-esp32c3-rust',
+  },
   // {
   //   title: 'The Time Machine',
   //   description: `Imagine being able to travel back in time or to the future. Simple turn the knob
